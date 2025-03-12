261d588e3221b3f1bac33c988888e7899caefebd
AssiarDev <halibaraissa@gmail.com>
2025-03-12 10:23:10 +0100

    route test

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
+// Connexion à la db
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_0hDFSg7QmuCn@ep-gentle-shape-a98o7km5-pooler.gwc.azure.neon.tech/neondb?sslmode=require",
+    ssl: {
+        rejectUnauthorized: false
+    },
+});
+
+// Route test pour savoir si les requêtes vers la db sont fonctionnel
+app.get('/test', async (req, res) => {
+    try{
+        const result = await pool.query('SELECT title FROM product');
+        console.log(result.rows);
+        res.json(result.rows);
+    } catch(e) {
+        console.error('Erreur serveur interne', e.message);
+        res.status(500).json({error: 'Erreur interne du serveur'})
+    }
+    
+})
+
+
 app.use(cors());
 app.use(express.json());
 
