99fc141ef30454e4c5b524872cfdb6a9eca7c912
Jude Abraham <jude.abraham@epitech.eu>
2025-03-13 15:35:38 +0100

    [ADD] levenshtein and case insensitive for search bar

--- routes/search_bar.js
+++ routes/search_bar.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../connect_db');
 
-router.get('/search', async (req, res) => {
+router.get('/search', async (req, res) => {     //fonction pour la bar de recherche
     try {
+        //l'url ressemblera a ".../search?q=smartphone" (exemple) donc on fait un get de 'q'
         const searchTerm = req.query.q;
 
         if (!searchTerm) {
             return res.status(400).json({ message: "Search term is required" });
         }
 
-        // PostgreSQL query using Levenshtein distance to find closest matches
+        //requete SQL pour chercher le produit soit par methode leveshtein pour ne pas trop pénaliser les fautes de frappes soit en cherchant le bout de mot écrit dans la bar de recherche dans les noms des produits
         const result = await pool.query(
             `SELECT *, levenshtein(lower(title), lower($1)) AS distance
              FROM product
@@ -19,6 +20,7 @@ router.get('/search', async (req, res) => {
              ORDER BY distance ASC`,
             [searchTerm, `%${searchTerm}%`]
         );
+        //levenshtein ne peut pas comparer un bout de mot a un mot entier donc on utilise les 2 methodes pour optimiser la recherche
 
         res.json(result.rows);
     } catch (error) {
